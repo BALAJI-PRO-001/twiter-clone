c03b55245724c56a46b6e583e04af0083c2ca36f
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-01-05 21:30:33 +0530

    Feat: sanitizeUserAndFormat.

--- backend/src/lib/utils/common.ts
+++ backend/src/lib/utils/common.ts
@@ -1,3 +1,4 @@
+import { ReadStream } from 'tty';
 import { HTTPError } from '../types';
 
 
@@ -11,4 +12,21 @@ export function createHTTPError(statusCode: number, message: string): HTTPError 
   const error = new Error(message) as HTTPError;
   error.statusCode = statusCode;
   return error;
+}
+
+
+
+export function sanitizeUserAndFormat(user: any) {
+  return {
+    id: user._id,
+    username: user.username,
+    fullName: user.fullName,
+    email: user.email,
+    followers: user.followers,
+    following: user.following,
+    profileImgURL: user.profileImgURL,
+    coverImgURL: user.coverImgURL,
+    bio: user.bio,
+    links: user.links
+  }
 }
