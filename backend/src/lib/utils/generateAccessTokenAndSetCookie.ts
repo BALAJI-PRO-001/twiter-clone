a025a640704385034f25e114228777d4e8bab201
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-01-05 19:38:52 +0530

    Fix:  Cookie expires date and time.

--- backend/src/lib/utils/generateAccessTokenAndSetCookie.ts
+++ backend/src/lib/utils/generateAccessTokenAndSetCookie.ts
@@ -2,20 +2,27 @@ import jwt from 'jsonwebtoken';
 import { Response } from 'express';
 
 
-
-export async function generateAccessTokenAndSetCookie(
+export function generateAccessTokenAndSetCookie(
   res: Response, 
   name: string,
-  payload: string | Buffer | object,
+  payload: object,
   options: {
     httpOnly?: boolean,
-    maxAge: number,
-    sameSite: boolean,
-    secure: boolean 
+    maxAge?: number,
+    sameSite?: boolean,
+    secure?: boolean 
   }
-) {
+): void {
   const DEFAULT_MAX_AGE = 15 * 24 * 60 * 60 * 1000; 
-  const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY as string, { expiresIn: '15d' }); res.cookie(name, accessToken, {
+  const EXPIRES_IN: any = options.maxAge 
+    ? options.maxAge / 1000 
+    : DEFAULT_MAX_AGE / 1000;
+
+  const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY as string, { 
+    expiresIn: EXPIRES_IN 
+  });
+
+  res.cookie(name, accessToken, {
     httpOnly: options.httpOnly ? true : false,
     maxAge: options.maxAge ? options.maxAge : DEFAULT_MAX_AGE, 
     sameSite: options.sameSite ? options.sameSite : false,
