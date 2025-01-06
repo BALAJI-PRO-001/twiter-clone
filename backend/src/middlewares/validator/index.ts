7e636eb7fef3a55906b61359b046e6d8414bb43d
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-01-06 20:49:25 +0530

    Feat: Mongodb id validator.

--- backend/src/middlewares/validator/index.ts
+++ backend/src/middlewares/validator/index.ts
@@ -1,10 +1,44 @@
+import { Request, Response, NextFunction } from 'express';
 import {
   validateNewUserData,
   validateUserCredentials
 } from './userDataValidator';
+import { body, param, validationResult } from 'express-validator';
+import { createHTTPError } from '../../lib/utils/common';
+
+
+async function validateId(
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+): Promise<void> {
+  await param('id')
+    .optional()
+    .isMongoId()
+    .run(req);
+
+  let result = validationResult(req);
+  if (!result.isEmpty()) {
+    return next(createHTTPError(400, 'Invalid mongodb id in url param.'));
+  }
+
+  await body(['id', 'followerId'])
+    .optional()
+    .isMongoId()
+    .run(req);
+
+  result = validationResult(req);
+  if (!result.isEmpty()) {
+    return next(createHTTPError(400, 'Invalid mongodb id in body.'));
+  }
+
+  next();
+}
+
 
 
 export {
   validateNewUserData,
-  validateUserCredentials
+  validateUserCredentials,
+  validateId
 };
