0e2753ac3baa9e00d55157fe85f06aabaedfb3a9
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-01-06 11:02:24 +0530

    Feat: Verify user access token function.

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,15 +1,23 @@
 import express from 'express';
-import { signup, login, logout } from '../controllers/auth.controller';
+import { 
+  signup, 
+  login, 
+  logout, 
+  getAuthenticatedUser 
+} from '../controllers/auth.controller';
 import { 
   validateNewUserData, 
-  validateUserCredentials 
+  validateUserCredentials
 } from '../middlewares/validator';
+import { verifyUserAccessToken } from '../middlewares/verifyUserAccessToken';
+
 
 const router = express.Router();
 
 router.post('/signup', validateNewUserData, signup)
       .post('/login', validateUserCredentials, login);
 
-router.get('/logout', logout);
+router.get('/logout', logout)
+      .get('/user/authenticated',  verifyUserAccessToken, getAuthenticatedUser);
 
 export default router;
