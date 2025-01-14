413e35ce15363e5910cb762812472246cf7332c6
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-01-14 10:58:56 +0530

    refactor: default values and if conditions.

--- backend/src/lib/utils/common.ts
+++ backend/src/lib/utils/common.ts
@@ -1,14 +1,20 @@
-import { ReadStream } from 'tty';
 import { HTTPError } from '../types';
+import { 
+  StatusCodes as STATUS_CODES, 
+  getReasonPhrase 
+} from 'http-status-codes';
 
 
-export function extractErrorMessage(error: Error | any) {
+export function extractErrorMessage(error: Error | any): string {
   return error instanceof Error ? error.message : String(error);
 }
 
 
 
-export function createHTTPError(statusCode: number, message: string): HTTPError {
+export function createHTTPError(
+  statusCode: number = STATUS_CODES.INTERNAL_SERVER_ERROR,
+  message: string = getReasonPhrase(STATUS_CODES.INTERNAL_SERVER_ERROR)  
+): HTTPError {
   const error = new Error(message) as HTTPError;
   error.statusCode = statusCode;
   return error;
@@ -17,6 +23,24 @@ export function createHTTPError(statusCode: number, message: string): HTTPError 
 
 
 export function sanitizeUserAndFormat(user: any) {
+  const requiredFields = [
+    '_id', 'username', 'fullName', 'email',
+    'followers', 'following', 'profileImgURL',
+    'coverImgURL', 'bio', 'link'
+  ];
+  const message = `The provided user data is invalid and cannot be formatted or sanitized.`;
+
+  if (typeof user !== 'object' || user === null || Array.isArray(user)) {
+    throw new Error(message);
+  }
+
+  let { password:_, ...userFields } = user;
+  userFields = Object.keys(userFields);
+
+  if (requiredFields.toString() !== userFields.toString()) {
+    throw new Error(message);
+  }
+
   return {
     _id: user._id,
     username: user.username,
@@ -27,6 +51,6 @@ export function sanitizeUserAndFormat(user: any) {
     profileImgURL: user.profileImgURL,
     coverImgURL: user.coverImgURL,
     bio: user.bio,
-    links: user.links
+    link: user.link
   }
 }
