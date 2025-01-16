e87494eb580083d62aa97f22a2ff8dc733341e97
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-01-16 19:43:41 +0530

    chore(format): intent spaces.

--- client/src/pages/auth/signup/index.jsx
+++ client/src/pages/auth/signup/index.jsx
@@ -1,97 +1,102 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-import XSvg from '../../../components/svgs/X';
+import XSvg from "../../../components/svgs/X";
 
-import { MdOutlineMail } from 'react-icons/md';
-import { FaUser } from 'react-icons/fa';
-import { MdPassword } from 'react-icons/md';
-import { MdDriveFileRenameOutline } from 'react-icons/md';
+import { MdOutlineMail } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { MdPassword } from "react-icons/md";
+import { MdDriveFileRenameOutline } from "react-icons/md";
 
 const Signup = () => {
-	const isError = false;
-	const [formData, setFormData] = useState({
-		username: '',
-		fullName: '',
-		email: '',
-		password: ''
-	});
+  const isError = false;
+  const [formData, setFormData] = useState({
+    username: "",
+    fullName: "",
+    email: "",
+    password: "",
+  });
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-	const handleInputChange = (e) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
-	};
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-	return (
-		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
-			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className=' lg:w-2/3 fill-white' />
-			</div>
-			<div className='flex-1 flex flex-col justify-center items-center'>
-				<form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<XSvg className='w-24 lg:hidden fill-white' />
-					<h1 className='text-4xl font-extrabold text-white'>Join Today.</h1>
-					<label className='input input-bordered rounded flex items-center gap-2 mt-2'>
-						<FaUser />
-						<input
-							type='text'
-							className='grow'
-							placeholder='Username'
-							name='username'
-							onChange={handleInputChange}
-							value={formData.username}
-						/>
-					</label>
-					<div className='flex gap-4 flex-wrap'>
-						<label className='input input-bordered rounded flex items-center gap-2 flex-1'>
-							<MdDriveFileRenameOutline />
-							<input
-								type='text'
-								className='grow '
-								placeholder='Full Name'
-								name='fullName'
-								onChange={handleInputChange}
-								value={formData.fullName}
-							/>
-						</label>
-						<label className='input input-bordered rounded flex items-center gap-2 flex-1'>
-							<MdOutlineMail />
-							<input
-								type='text'
-								className='grow'
-								placeholder='Email'
-								name='email'
-								onChange={handleInputChange}
-								value={formData.email}
-							/>
-						</label>
-					</div>
-					<label className='input input-bordered rounded flex items-center gap-2'>
-						<MdPassword />
-						<input
-							type='password'
-							className='grow'
-							placeholder='Password'
-							name='password'
-							onChange={handleInputChange}
-							value={formData.password}
-						/>
-					</label>
-					<button className='btn rounded-full btn-primary text-white'>Sign up</button>
-					{isError && <p className='text-red-500'>Something went wrong</p>}
-				</form>
-				<div className='flex flex-wrap gap-2 mt-4 font-bold'>
-					<p className='text-white text-lg'>Already have an account?</p>
-					<Link to='/login'>
-						<span className='text-primary text-lg'>Login</span>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="max-w-screen-xl mx-auto flex h-screen px-10">
+      <div className="flex-1 hidden lg:flex items-center  justify-center">
+        <XSvg className=" lg:w-2/3 fill-white" />
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <form
+          className="lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col"
+          onSubmit={handleSubmit}
+        >
+          <XSvg className="w-24 lg:hidden fill-white" />
+          <h1 className="text-4xl font-extrabold text-white">Join Today.</h1>
+          <label className="input input-bordered rounded flex items-center gap-2 mt-2">
+            <FaUser />
+            <input
+              type="text"
+              className="grow"
+              placeholder="Username"
+              name="username"
+              onChange={handleInputChange}
+              value={formData.username}
+            />
+          </label>
+          <div className="flex gap-4 flex-wrap">
+            <label className="input input-bordered rounded flex items-center gap-2 flex-1">
+              <MdDriveFileRenameOutline />
+              <input
+                type="text"
+                className="grow "
+                placeholder="Full Name"
+                name="fullName"
+                onChange={handleInputChange}
+                value={formData.fullName}
+              />
+            </label>
+            <label className="input input-bordered rounded flex items-center gap-2 flex-1">
+              <MdOutlineMail />
+              <input
+                type="text"
+                className="grow"
+                placeholder="Email"
+                name="email"
+                onChange={handleInputChange}
+                value={formData.email}
+              />
+            </label>
+          </div>
+          <label className="input input-bordered rounded flex items-center gap-2">
+            <MdPassword />
+            <input
+              type="password"
+              className="grow"
+              placeholder="Password"
+              name="password"
+              onChange={handleInputChange}
+              value={formData.password}
+            />
+          </label>
+          <button className="btn rounded-full btn-primary text-white">
+            Sign up
+          </button>
+          {isError && <p className="text-red-500">Something went wrong</p>}
+        </form>
+        <div className="flex flex-wrap gap-2 mt-4 font-bold">
+          <p className="text-white text-lg">Already have an account?</p>
+          <Link to="/login">
+            <span className="text-primary text-lg">Login</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
