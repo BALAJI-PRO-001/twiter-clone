964035bdc085d5ab38818eae299cadf39e35a5f9
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-01-17 20:20:09 +0530

    feat(input): add validation listener.

--- client/src/pages/auth/signup/index.jsx
+++ client/src/pages/auth/signup/index.jsx
@@ -8,8 +8,17 @@ import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
 
+import { 
+  validateUsername,
+  validateFullName,
+  validateEmail,
+  validatePassword
+} from '../../../utils/validator';
+
+
 const Signup = () => {
-  const isError = false;
+  const isError = true;
+  const [errMessage, setErrMessage] = useState('');
   const [formData, setFormData] = useState({
     username: "",
     fullName: "",
@@ -21,9 +30,52 @@ const Signup = () => {
     e.preventDefault();
   };
 
+
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+
+  const handleInputValidation =  async (e) => {
+    e.preventDefault();
+    
+    if (e.target.id === 'username') {
+      const { isValid, message } = await validateUsername(e.target.value);
+      if (!isValid) {
+        return setErrMessage(message);
+      } else {
+        return setErrMessage('');
+      }
+    }
+
+    if (e.target.id === 'fullName') {
+      const { isValid, message } = await validateFullName(e.target.value);
+      if (!isValid) {
+        return setErrMessage(message);
+      } else {
+        return setErrMessage('');
+      }
+    }
+
+    if (e.target.id === 'email') {
+      const { isValid, message } = await validateEmail(e.target.value);
+      if (!isValid) {
+        return setErrMessage(message);
+      } else {
+        return setErrMessage('');
+      }
+    }
+
+    if (e.target.id === 'password') {
+      const { isValid, message } = await validatePassword('Password', e.target.value);
+      if (!isValid) {
+        return setErrMessage(message);
+      } else {
+        return setErrMessage('');
+      }
+    }
+  }
+
 
   return (
     <div className="max-w-screen-xl mx-auto flex h-screen px-10">
@@ -40,11 +92,13 @@ const Signup = () => {
           <label className="input input-bordered rounded flex items-center gap-2 mt-2">
             <FaUser />
             <input
+              id="username"
               type="text"
               className="grow"
               placeholder="Username"
               name="username"
               onChange={handleInputChange}
+              onKeyUp={handleInputValidation}
               value={formData.username}
             />
           </label>
@@ -52,22 +106,26 @@ const Signup = () => {
             <label className="input input-bordered rounded flex items-center gap-2 flex-1">
               <MdDriveFileRenameOutline />
               <input
+                id="fullName"
                 type="text"
                 className="grow "
                 placeholder="Full Name"
                 name="fullName"
                 onChange={handleInputChange}
+                onKeyUp={handleInputValidation}
                 value={formData.fullName}
               />
             </label>
             <label className="input input-bordered rounded flex items-center gap-2 flex-1">
               <MdOutlineMail />
               <input
+                id="email"
                 type="text"
                 className="grow"
                 placeholder="Email"
                 name="email"
                 onChange={handleInputChange}
+                onKeyUp={handleInputValidation}
                 value={formData.email}
               />
             </label>
@@ -75,23 +133,25 @@ const Signup = () => {
           <label className="input input-bordered rounded flex items-center gap-2">
             <MdPassword />
             <input
+              id="password"
               type="password"
               className="grow"
               placeholder="Password"
               name="password"
               onChange={handleInputChange}
+              onKeyUp={handleInputValidation}
               value={formData.password}
             />
           </label>
           <button className="btn rounded-full btn-primary text-white">
             Sign up
           </button>
-          {isError && <p className="text-red-500">Something went wrong</p>}
+          {isError && <p className="text-red-500">{errMessage}</p>}
         </form>
-        <div className="flex flex-wrap gap-2 mt-4 font-bold">
+        <div className="flex flex-wrap gap-2 mt-4">
           <p className="text-white text-lg">Already have an account?</p>
           <Link to="/login">
-            <span className="text-primary text-lg">Login</span>
+            <span className="text-primary text-lg font-bold">Login</span>
           </Link>
         </div>
       </div>
