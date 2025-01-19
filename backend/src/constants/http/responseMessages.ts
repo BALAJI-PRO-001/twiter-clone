1f739558cfca2c452023b06be48fe977b6d050de
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-01-19 21:10:39 +0530

    feat: add reuseable constant http response messages for controllers.

---
+++ backend/src/constants/http/responseMessages.ts
@@ -0,0 +1,37 @@
+export const AUTHENTICATION_MESSAGES = {
+  USER: {
+    SIGN_UP: 'Account created successfully.',
+    LOGGED_IN: 'User logged in successfully.',
+    LOGGED_OUT: 'Logged out successfully.',
+    AUTH_USER_INFO: 'Authenticated user information retrieved successfully.'
+  }
+}
+
+
+export const POST_MESSAGES = {
+  CREATED: 'The post was created successfully.',
+  LIKED: 'The post was liked successfully.',
+  UNLIKED: 'The post was unliked successfully.',
+  COMMENTED: 'The post was commented on successfully.',
+  GET_ALL_POSTS: 'All posts retrieved successfully.',
+  GET_LIKED_POSTS: 'All liked posts retrieved successfully.',
+  GET_FOLLOWING_POSTS: 'Following posts retrieved successfully.',
+  GET_OWN_POSTS: 'Your posts retrieved successfully.'
+};
+
+
+
+export const USER_MESSAGES = {
+  GET_USER_INFO: 'User information retrieved successfully.',
+  UNFOLLOWED: 'User unfollowed successfully.',
+  FOLLOWED: 'User followed successfully.',
+  GET_SUGGESTED_USERS: 'Suggested users retrieved successfully.',
+  UPDATED: 'User information updated successfully.'
+};
+
+
+
+
+export const NOTIFICATION_MESSAGES = {
+  GET_ALL_NOTIFICATIONS: 'All notifications retrieved successfully.'
+};
