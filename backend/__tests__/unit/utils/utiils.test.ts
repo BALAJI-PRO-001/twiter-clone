f3532901906bb7750d973e0986c8fdfb05fd9328
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-01-19 21:06:21 +0530

    refactor: removed sanitizeUserAndFormat function test code.

--- backend/__tests__/unit/utils/utiils.test.ts
+++ backend/__tests__/unit/utils/utiils.test.ts
@@ -3,7 +3,6 @@ import { StatusCodes as STATUS_CODES, getReasonPhrase } from 'http-status-codes'
 import { 
   createHTTPError,
   extractErrorMessage,
-  sanitizeUserAndFormat
 } from '../../../src/lib/utils/common';
 
 
@@ -46,46 +45,6 @@ describe('Utility Functions Tests', () => {
       expect(message).toBeDefined();
       expect(message).not.toBeNull();
       expect(message).toBe('400');
-    });
-  });
-
-
-  describe('Test 3: sanitizeUserAndFormat', () => {
-    const user = {
-      _id: '6782840babd4d39bb35fca51',
-      username: 'Ram',
-      fullName: 'Ram',
-      email: 'ram@gmail.com',
-      password: 'ram!@#$',
-      followers: [],
-      following: [],
-      profileImgURL: 'https://example.com',
-      coverImgURL: 'https://example.com',
-      bio: 'Nothing',
-      link: 'https://example.com'
-    };
-
-    const errorMessage = 'The provided user data is invalid and cannot be formatted or sanitized.';
-
-    it ('should throw an error with invalid user data', () => {
-      expect(() => sanitizeUserAndFormat(100)).toThrow(errorMessage);
-      expect(() => sanitizeUserAndFormat('')).toThrow(errorMessage);
-      expect(() => sanitizeUserAndFormat([10, 20])).toThrow(errorMessage);
-    });
-
-    it ('should throw an error with missing required fields', () => {
-      const invalidUserData = { username: 'Ram' };
-      expect(() => sanitizeUserAndFormat(invalidUserData)).toThrow(errorMessage);
-    });
-
-    it ('should return valid sanitize and formatted user data.', () => {
-      const sanitizedUser = sanitizeUserAndFormat(user);
-      expect(sanitizedUser).toBeDefined();
-      expect(sanitizedUser).not.toBeNull();
-      expect(typeof sanitizedUser).toBe('object');
-
-      const { password:_, ...rest } = user;
-      expect(sanitizedUser).toEqual(rest); 
     });
   });
 });
